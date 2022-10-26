0b9805486c941cb3dadfc3d67313e5a1f0ea6f5a
SUNQIYUA“git config --global user.email 2655985941@qq.comssh-keygen -t rsassh-keygen -t rsagit config --global user.name SUNQIYUA“ <2655985941@qq.com>
2022-10-27 00:49:40 +0800

    Modify main

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+int main(void)
+{
+	float a = 2;
+	const double h = 1e-3;
+	double x = 0;
+	printf("please enter the a \n");
+	//scanf("%f", &a);
+	float z = a;
+	float y = a;
+	printf("x     y \n");
+	printf("%f     %f\n", x, y);
+	x = x + h;
+	y = (1-h/2) *y/ (1+h/2) ;
+	printf("%f     %f\n", x, y);
+	double k ;	
+	k = z - y;
+	while (k >= 0.001)
+	{
+		x = x + h;
+		y = (1-h/2) / (1+h/2) * y;
+		z = (1-h/2) / (1+h/2) * z;
+		k = z - y;
+		printf("%f     %f\n", x, y);
+		
+		
+		}
+	
+	
 	return 0;
+	
 }
 
